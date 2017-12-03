22a35d570db03b02060b195ecc713a1ba1b845e7
DESKTOP-O5ARVSK\78123 <781230111@qq.com>
2017-12-04 01:17:43 +0800

    拉拽功能添加

---
+++ VRtest/Assets/Scripts/FllowController.cs
@@ -0,0 +1,48 @@
+﻿namespace VRTK.Examples
+{
+    using UnityEngine;
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class FllowController : MonoBehaviour
+    {
+
+        public GameObject leftController;
+        public GameObject rightController;
+        private GameObject target;
+        public VRTK_ControllerPointerEvents_ListenerExample r;
+        public float factor = 1.0f;
+
+        Vector3 ControllerOldPos;
+        Vector3 ControllerNewPos;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            ControllerNewPos = rightController.transform.position;
+            if (r.fllow)
+            {
+                if (r.target != null)
+                {
+                    target = r.target;
+                }
+                if (ControllerOldPos != null)
+                {
+                    float x = target.transform.position.x;
+                    float z = target.transform.position.z;
+                    float y = target.transform.position.y;
+                    target.transform.position = new Vector3(x, y + (ControllerNewPos.y-ControllerOldPos.y)*factor, z);
+                    //target.transform.position += ControllerNewPos - ControllerOldPos;
+                }
+            }
+            ControllerOldPos = rightController.transform.position;
+        }
+    }
+}
--- VRtest/Assets/VRTK/Examples/ExampleResources/Scripts/VRTK_ControllerPointerEvents_ListenerExample.cs
+++ VRtest/Assets/VRTK/Examples/ExampleResources/Scripts/VRTK_ControllerPointerEvents_ListenerExample.cs
@@ -6,6 +6,9 @@
     {
         public HighlightTarget highlightTarget;
         public bool showHoverState = false;
+        private bool triggerPressed = false;
+        public bool fllow = false;
+        public GameObject target;
 
         private void Start()
         {
@@ -14,7 +17,8 @@
                 VRTK_Logger.Error(VRTK_Logger.GetCommonMessage(VRTK_Logger.CommonMessageKeys.REQUIRED_COMPONENT_MISSING_FROM_GAMEOBJECT, "VRTK_ControllerPointerEvents_ListenerExample", "VRTK_DestinationMarker", "the Controller Alias"));
                 return;
             }
-
+            GetComponent<VRTK_ControllerEvents>().TriggerPressed += new ControllerInteractionEventHandler(DoTriggerPressed);
+            GetComponent<VRTK_ControllerEvents>().TriggerReleased += new ControllerInteractionEventHandler(TriggerReleased);
             //Setup controller event listeners
             GetComponent<VRTK_DestinationMarker>().DestinationMarkerEnter += new DestinationMarkerEventHandler(DoPointerIn);
             if (showHoverState)
@@ -24,28 +28,49 @@
             GetComponent<VRTK_DestinationMarker>().DestinationMarkerExit += new DestinationMarkerEventHandler(DoPointerOut);
             GetComponent<VRTK_DestinationMarker>().DestinationMarkerSet += new DestinationMarkerEventHandler(DoPointerDestinationSet);
         }
-
+        private void DoTriggerPressed(object sender, ControllerInteractionEventArgs e)
+        {
+            triggerPressed = true;
+            
+        }
+        private void TriggerReleased(object sender, ControllerInteractionEventArgs e)
+        {
+            triggerPressed = false;
+           
+        }
         private void DebugLogger(uint index, string action, Transform target, RaycastHit raycastHit, float distance, Vector3 tipPosition)
         {
             string targetName = (target ? target.name : "<NO VALID TARGET>");
             string colliderName = (raycastHit.collider ? raycastHit.collider.name : "<NO VALID COLLIDER>");
-            VRTK_Logger.Info("Controller on index '" + index + "' is " + action + " at a distance of " + distance + " on object named [" + targetName + "] on the collider named [" + colliderName + "] - the pointer tip position is/was: " + tipPosition);
+           // VRTK_Logger.Info("Controller on index '" + index + "' is " + action + " at a distance of " + distance + " on object named [" + targetName + "] on the collider named [" + colliderName + "] - the pointer tip position is/was: " + tipPosition);
         }
 
         private void DoPointerIn(object sender, DestinationMarkerEventArgs e)
         {
+            target = e.target.gameObject;
             highlightTarget.SetHighlightMaterial(e.target);
             DebugLogger(VRTK_ControllerReference.GetRealIndex(e.controllerReference), "POINTER IN", e.target, e.raycastHit, e.distance, e.destinationPosition);
         }
 
         private void DoPointerOut(object sender, DestinationMarkerEventArgs e)
         {
+            target = null;
             highlightTarget.SetInitailMaterial(e.target);
             DebugLogger(VRTK_ControllerReference.GetRealIndex(e.controllerReference), "POINTER OUT", e.target, e.raycastHit, e.distance, e.destinationPosition);
         }
 
         private void DoPointerHover(object sender, DestinationMarkerEventArgs e)
         {
+            if(triggerPressed)
+            {
+                fllow = true;
+                
+            }
+            if(!triggerPressed)
+            {
+                fllow = false;
+                target = null;
+            }
             DebugLogger(VRTK_ControllerReference.GetRealIndex(e.controllerReference), "POINTER HOVER", e.target, e.raycastHit, e.distance, e.destinationPosition);
         }
 
