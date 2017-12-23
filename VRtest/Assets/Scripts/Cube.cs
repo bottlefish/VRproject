7231ceeeb6971fbba6424cc53771cd5283afc442
CHINA-710192156\Administrator <3038442038@qq.com>
2017-12-23 10:14:46 +0800

    加了方块的三个变量

--- VRtest/Assets/Scripts/Cube.cs
+++ VRtest/Assets/Scripts/Cube.cs
@@ -5,36 +5,47 @@ using UnityEngine;
 public class Cube : MonoBehaviour
 {
 
+    public int num = 0; //cube编号
     private MeshRenderer render;
     public Material highLightMat;
-    public float factor = 1.5f;
+    public float factor = 1.5f; //上升速率
     Vector3 ControllerOldPos;
     Vector3 ControllerNewPos;
     private Material originMat;
     public bool isTriggerMe = false;
-    private GameObject rightController;
+    private GameObject rightController;    
+    public CubeParent parent;
+
 
     void Awake()
     {
         rightController = GameObject.FindWithTag("RightController");
         render = GetComponent<MeshRenderer>();
         originMat = render.material;
+        parent = GetComponentInParent<CubeParent>();
+        parent.cube.Add(this);
     }
 
 
     void Update()
-    {
+    {      
         ControllerNewPos = rightController.transform.position;
         if (ControllerOldPos != null)
         {
             if (isTriggerMe)
             {
-                float x = transform.position.x;
-                float z = transform.position.z;
-                float y = transform.position.y;
-                transform.position = new Vector3(x, y + (ControllerNewPos.y - ControllerOldPos.y) * factor, z);
+                float x = transform.parent.position.x;
+                float z = transform.parent.position.z;
+                float y = transform.parent.position.y;
+                transform.parent.position = new Vector3(x, y + (ControllerNewPos.y - ControllerOldPos.y) * factor, z);
+                
+            }
+            else
+            {
+                RaycastDown();                
             }
         }
+
         ControllerOldPos = rightController.transform.position;
     }
 
@@ -51,6 +62,37 @@ public class Cube : MonoBehaviour
     }
 
 
+   public bool RaycastDown() { //向下检测
+        Ray ray = new Ray(transform.position,-transform.up);
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo, 8))
+        {
+            if (hitInfo.collider.tag == "Ground")
+            {
+                parent.nowCount = num;
+                return true;
+            }
+       
+        }
+        return false;
+
+    }
+
+   public int RaycastUp() //向上检测
+   {
+       Ray ray = new Ray(transform.position, transform.up);
+       RaycastHit hitInfo;
+       if (Physics.Raycast(ray, out hitInfo, 8)) //第八层 Ground
+       {
+           if (hitInfo.collider.tag == "Ground")
+           {
+               return num;
+           }
+
+       }
+       return 0;
+
+   }
 
 }
 
---
+++ VRtest/Assets/Scripts/CubeParent.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeParent : MonoBehaviour {
+    public int childCount = 3;//有几个子物体
+    public int originCount = 0;//默认初始0层
+    public  int nowCount = 0; //现在状态
+    private Vector3 originPos; //初始位置
+    /*
+     * 4
+     * 3
+     * 2
+     * 1
+     * 0  //默认状态
+     * -1 //特殊情况
+     * -2 //特殊情况
+     * -3 //特殊情况
+     * -4 //特殊情况
+     */
+    [HideInInspector]
+    public  List<Cube> cube = new List<Cube>(); 
+    public float length = 1; //默认高度大小
+
+    void Awake() {
+        originPos = transform.position;
+        transform.position += new Vector3(0,originCount * length,0);
+    }
+
+    public void Update()
+    {
+ 
+        foreach (var a in cube) {
+            if (a.isTriggerMe) 
+                return;
+            if (a.RaycastUp() == 1) {
+                nowCount = 0;
+            }
+        }
+        transform.position = originPos + new Vector3(0, nowCount * length);
+    }
+}
